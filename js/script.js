5b1d9a471426783b8acea2d1dc844dab62c388d0
Michele Lisignoli <michele@lisignoli.com>
2021-02-25 15:08:28 +0100

    name - surname - favourite color

---
+++ js/script.js
@@ -0,0 +1,5 @@
+var name = prompt("What's your name?");
+var surname = prompt("What's your surname?");
+var favouriteColor = prompt("Witch is your favourite color?");
+
+document.getElementById("aboutYou").innerHTML = "You are" + " " + name + " " + surname + " " + "and your favourite color is:" + " " + favouriteColor;
